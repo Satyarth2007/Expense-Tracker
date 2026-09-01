1f6f0837aa303c8bf108636b01f3e6a2edcd288c
Satyarth2007 <satyarthshukla2007@gmail.com>
2026-09-01 18:15:47 +0530

    Updated the recurring field

--- Server/src/workers/recurringWorker.ts
+++ Server/src/workers/recurringWorker.ts
@@ -1,11 +1,11 @@
-import { Worker } from "bullmq";
+import { Job, Worker } from "bullmq";
 import { bullmqConnection } from "../config/bullmqRedis.js";
 import { RECURRING_QUEUE_NAME } from "../queues/recurringQueue.js";
 import { processRecurringSweep } from "../jobs/processRecurringSweep.js";
 
 export const recurringWorker = new Worker(
   RECURRING_QUEUE_NAME,
-  async (job) => {
+  async (job: Job) => {
     console.log(`Running recurring sweep job (id: ${job.id})`);
     return await processRecurringSweep();
   },
